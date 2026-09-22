09276f847e547e66ed2382c4443b51efd7409b01
ТВОЁ_ИМЯ <email@example.com>
2026-09-22 21:04:24 +0500

    assigmnet2

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import abstractfactory.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== PART A: FACTORY METHOD ===");
-        // Клиент работает только через абстрактные классы/интерфейсы
         DeviceFactory lampFactory = new LampFactory();
         lampFactory.operateDevice();
 
@@ -12,13 +11,11 @@ public class Main {
         thermostatFactory.operateDevice();
 
         System.out.println("\n=== PART B: ABSTRACT FACTORY ===");
-        // Выбираем вендора в одном месте
         SmartHomeFactory vendorFactory = new XiaomiFactory(); 
         
         SmartDevice device = vendorFactory.createDevice();
         SmartController controller = vendorFactory.createController();
 
-        // Проверяем совместимость всей экосистемы
         controller.control(device);
     }
 }
